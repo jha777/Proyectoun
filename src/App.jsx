37db583b27b1b802203f6d65fcea4e27e825121d
jha777 <89705204+jha777@users.noreply.github.com>
2025-07-03 17:14:27 -0500

    perfum

--- src/App.jsx
+++ src/App.jsx
@@ -92,12 +92,12 @@ const mockBusinesses = [
   },
   {
     id: 10,
-    name: 'Publica tu Negocio',
-    category: 'Librerías',
-    city: 'Florida',
-    logoUrl: '/images/negocios/publicatunegocio.png',
-    description: 'TUKU Express es la WEB donde descubres emprendimientos Colombianos por Ciudad/Categoría y entras directo a sus páginas web.',
-    websiteUrl: 'https://wa.me/3225212078?text=Hola,%20quiero%20Publicar%20mi%20Negocio',
+    name: 'Lucho Perfumería',
+    category: 'Salud y Belleza',
+    city: 'Bucaramanga',
+    logoUrl: '/images/negocios/luchoperfumeria.png',
+    description: 'Lucho Perfumería es tu destino en Bucaramanga para encontrar las mejores fragancias.',
+    websiteUrl: 'https://luchoperfumeria.com.co/',
   },
   {
     id: 11,
